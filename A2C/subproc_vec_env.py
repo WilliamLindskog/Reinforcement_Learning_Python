e7b12564217fef9e0fefad64a2389fa4d8876cbe
WilliamLindskog <williamlindskog@outlook.com>
2021-02-21 21:17:37 +0100

    Adding for Subproc

--- A2C/subproc_vec_env.py
+++ A2C/subproc_vec_env.py
@@ -5,3 +5,51 @@ def worker(remote, parent_remote, env_fn_wrapper):
     parent_remote.close()
     env = env_fn_wrapper.x()
     while True:
+        cmd, data = remote.recv()
+        if cmd == 'step':
+            ob, reward, done, info = env.step(data)
+            if done:
+                ob = env.reset()
+            remote.send((ob, reward, done, info))
+        elif cmd == 'reset':
+            ob = env.reset()
+            remote.send(ob)
+        elif cmd == 'reset_task':
+            ob = env.reset_task()
+            remote.send(ob)
+        elif cmd == 'close':
+            remote.close()
+            break
+        elif cmd == 'get_spaces':
+            remote.send((env.action_space, env.observation_space))
+        elif cmd == 'get_id':
+            remote.send(env.spec.id)
+        else:
+            raise NotImplementedError
+
+class CloundpickleWrapper():
+    """
+    Uses cloudpickle to serialize contents (otherwise multiprocessing tries to use pickle)
+    """
+
+    def __init__(self, x):
+        self.x = x
+
+    def __getstate__(self):
+        import cloudpickle
+        return cloudpickle.dumps(self.x)
+
+    def __setstate__(self, ob):
+        import pickle
+        self.x = pickle.loads(ob)
+
+class SubprocVecEnv():
+    def __init__(self, env_fns):
+        """
+        :param env_fns: list of gym environments to run in subprocesses
+        """
+
+        self.closed = False
+        nenvs = len(env_fns)
+        self.remotes, self.work_remotes = zip(*[Pipe() for _ in range(nenvs)])
+        self.ps = [Process(target = worker, args=(work_remote, remote))]
